6dec339118385ff7d3c3792b60fd886c3df057e1
Minevitably <minevitably@gmail.com>
2026-02-12 22:00:53 +0800

    Q1688-CountOfMatchesInTournament trivial

---
+++ Q1694-ReformatPhoneNumber/src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+        Set<Character> charSet = new HashSet<>();
+        for (char ch: allowed.toCharArray()) {
+            charSet.add(ch);
+        }
+        int count = 0;
+
+        for (String word: words) {
+            boolean flag = true;
+            for (char ch: word.toCharArray()) {
+                if (!charSet.contains(ch)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
