9dd982724ac5b0e44a849dc044edde1e0daf08b6
Minevitably <minevitably@gmail.com>
2026-02-16 21:50:04 +0800

    Q1732-FindTheHighestAltitude trivial

---
+++ Q1732-FindTheHighestAltitude/src/main/java/org/example/Main.java
@@ -0,0 +1,33 @@
+package org.example;
+
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int maxAltitude = 0;
+        int n = gain.length;
+        int currentPointAltitude = 0;
+
+        for (int i = 0; i < n; i++) {
+            currentPointAltitude += gain[i];
+            maxAltitude = Math.max(maxAltitude, currentPointAltitude);
+        }
+
+        return maxAltitude;
+    }
+}
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
