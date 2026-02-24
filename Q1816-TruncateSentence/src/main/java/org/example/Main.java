9a5daac5568c78071aa809dfd9e1ae239e1bf42b
Minevitably <minevitably@gmail.com>
2026-02-24 21:44:46 +0800

    Q1816-TruncateSentence trivial

--- Q1816-TruncateSentence/src/main/java/org/example/Main.java
+++ Q1816-TruncateSentence/src/main/java/org/example/Main.java
@@ -1,32 +1,11 @@
 package org.example;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 class Solution {
-    private String removeLeadingZeros(String str) {
-        int n = str.length();
-        int i = 0;
-
-        while (i < n - 1 && str.charAt(i) == '0') {
-            i++;
-        }
-
-        return str.substring(i);
-    }
-    public int numDifferentIntegers(String word) {
-        Set<String> s = new HashSet<>();
-
-        word = word.replaceAll("[a-zA-Z]", " ");
-        String[] numStrs = word.split(" ");
-
-        for (String numStr: numStrs) {
-            if (!numStr.isEmpty()) {
-                s.add(removeLeadingZeros(numStr));
-            }
-        }
-
-        return s.size();
+    public String truncateSentence(String s, int k) {
+        String[] words = s.split(" ");
+        return String.join(" ", Arrays.copyOfRange(words, 0, k));
     }
 }
 
@@ -34,7 +13,6 @@ class Solution {
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        (new Solution()).numDifferentIntegers("a123bc34d8ef34");
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
