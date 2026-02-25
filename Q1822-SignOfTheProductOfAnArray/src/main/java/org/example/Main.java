13744197620eed30c8958ace93335540c5e5fa7d
Minevitably <minevitably@gmail.com>
2026-02-25 20:15:43 +0800

    Q1822-SignOfTheProductOfAnArray trivial

---
+++ Q1822-SignOfTheProductOfAnArray/src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+
+class Solution {
+    public int arraySign(int[] nums) {
+        int count = 0;
+
+        for (int num: nums) {
+            if (num == 0) {
+                return 0;
+            }
+            if (num < 0) {
+                count++;
+            }
+        }
+
+        if (count % 2 == 0) {
+            return 1;
+        }
+        return -1;
+    }
+}
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
