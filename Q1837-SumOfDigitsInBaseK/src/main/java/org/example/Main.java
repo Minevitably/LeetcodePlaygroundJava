f63ed30d394a4bcc7e4eb85bc7da10303d4ee408
Minevitably <minevitably@gmail.com>
2026-02-26 20:37:14 +0800

    Q1837-SumOfDigitsInBaseK trivial

--- Q1837-SumOfDigitsInBaseK/src/main/java/org/example/Main.java
+++ Q1837-SumOfDigitsInBaseK/src/main/java/org/example/Main.java
@@ -2,18 +2,15 @@ package org.example;
 
 
 class Solution {
-    public int minOperations(int[] nums) {
-        int n = nums.length;
-        int count = 0;
+    public int sumBase(int n, int k) {
+        int sum = 0;
 
-        for (int i = 1; i < n; i++) {
-            if (nums[i - 1] >= nums[i]) {
-                count += nums[i - 1] - nums[i] + 1;
-                nums[i] = nums[i - 1] + 1;
-            }
+        while (n > 0) {
+            sum += n % k;
+            n /= k;
         }
 
-        return count;
+        return sum;
     }
 }
 
