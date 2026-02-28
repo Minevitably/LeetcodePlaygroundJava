2992f8108dc27811b798a4f35c5809cab747cfe9
Minevitably <minevitably@gmail.com>
2026-03-01 00:45:06 +0800

    Q1859-SortingTheSentence trivial

---
+++ Q1863-SumOfAllSubsetXORTotals/src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int left = start;
+        int right = start;
+        int n = nums.length;
+        
+        while (left >= 0 || right < n) {
+            if ((left >= 0 && nums[left] == target) ||
+                (right < n && nums[right] == target)) {
+                return right - start;
+            }
+
+            left--;
+            right++;
+        }
+        
+        // never happen
+        return -1;
+    }
+}
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
