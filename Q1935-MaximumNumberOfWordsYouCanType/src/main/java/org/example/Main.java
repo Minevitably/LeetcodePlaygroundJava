0a8d7490239ce8e55baa4384d7414e388d50c5b5
Minevitably <minevitably@gmail.com>
2026-03-08 00:29:32 +0800

    Q1929-ConcatenationOfArray trivial

---
+++ Q1935-MaximumNumberOfWordsYouCanType/src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+
+class Solution {
+    public int countTriples(int n) {
+        int count = 0;
+
+        for (int a = 1; a <= n; a++) {
+            for (int b = 1; b <= n; b++) {
+                for (int c = 1; c <= n; c++) {
+                    if (a * a + b * b == c * c) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
