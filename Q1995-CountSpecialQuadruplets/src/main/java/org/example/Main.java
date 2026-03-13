203f4711fa4c0cdb4e47548cebc3ba9929b981d3
Minevitably <minevitably@gmail.com>
2026-03-14 00:30:32 +0800

    Q1995-CountSpecialQuadruplets trivial

--- Q1995-CountSpecialQuadruplets/src/main/java/org/example/Main.java
+++ Q1995-CountSpecialQuadruplets/src/main/java/org/example/Main.java
@@ -1,19 +1,24 @@
 package org.example;
 
-import java.util.Arrays;
 
 class Solution {
-    public int minimumDifference(int[] nums, int k) {
-        Arrays.sort(nums);
-
-        int minVal = nums[k - 1] - nums[0];
+    public int countQuadruplets(int[] nums) {
         int n = nums.length;
-        
-        for (int i = k; i < n; i++) {
-            minVal = Math.min(minVal, nums[i] - nums[i - (k - 1)]);
+        int count = 0;
+
+        for (int a = 0; a < n; a++) {
+            for (int b = a + 1; b < n; b++) {
+                for (int c = b + 1; c < n; c++) {
+                    for (int d = c + 1; d < n; d++) {
+                        if (nums[a] + nums[b] + nums[c] == nums[d]) {
+                            count++;
+                        }
+                    }
+                }
+            }
         }
 
-        return minVal;
+        return count;
     }
 }
 
