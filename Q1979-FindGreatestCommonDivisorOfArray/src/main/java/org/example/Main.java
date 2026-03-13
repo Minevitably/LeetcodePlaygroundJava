84d02817f1c7fd8984eafaf1c66c8c81f23d4206
Minevitably <minevitably@gmail.com>
2026-03-13 23:53:48 +0800

    Q1979-FindGreatestCommonDivisorOfArray trivial

---
+++ Q1979-FindGreatestCommonDivisorOfArray/src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+
+class Solution {
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+    public int findGCD(int[] nums) {
+        int maxVal = nums[0];
+        int minVal = nums[0];
+
+        for (int num: nums) {
+            maxVal = Math.max(maxVal, num);
+            minVal = Math.min(minVal, num);
+        }
+        return gcd(maxVal, minVal);
+    }
+}
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
