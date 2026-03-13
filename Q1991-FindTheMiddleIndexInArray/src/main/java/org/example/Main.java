cb6ff71484ed77d39a39ee8106ffc06af6621cfd
Minevitably <minevitably@gmail.com>
2026-03-14 00:28:54 +0800

    Q1991-FindTheMiddleIndexInArray trivial

---
+++ Q1991-FindTheMiddleIndexInArray/src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+
+
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+
+        for (int num: nums) {
+            sum += num;
+        }
+
+        int l = 0;
+        int r = sum;
+
+        for (int i = 0; i < n; i++) {
+            if (i - 1 >= 0) {
+                l += nums[i - 1];
+            }
+            r -= nums[i];
+            if (l == r) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
