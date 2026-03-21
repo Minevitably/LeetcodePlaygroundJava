3164ee25ec21767c9d0a8b82cd1329aa314775f4
Minevitably <minevitably@gmail.com>
2026-03-22 00:21:55 +0800

    Q2062-CountVowelSubstringsOfAString medium

---
+++ Q2062-CountVowelSubstringsOfAString/src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+
+class Solution
+{
+    public int countVowelSubstrings(String word)
+    {
+        int vow = 0;
+        int n = word.length();
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i < n-4; i++)
+        {
+            set.clear();
+            for(int j = i; j < n; j++)
+            {
+                char ch = word.charAt(j);
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                {
+                    set.add(ch);
+                    if(set.size() == 5)
+                        vow++;
+                }
+                else
+                    break;
+            }
+        }
+        return vow;
+    }
+}
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
---
+++ Q2068-CheckWhetherTwoStringsAreAlmostEquivalent/src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+
+class Solution {
+    public int smallestEqual(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 10 == nums[i]) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
