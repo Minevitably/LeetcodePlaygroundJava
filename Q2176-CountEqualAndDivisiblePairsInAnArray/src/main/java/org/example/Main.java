3861801ddcddcc8814455c995b67d8f16c14b7c3
Minevitably <minevitably@gmail.com>
2026-03-31 21:10:16 +0800

    Q2176-CountEqualAndDivisiblePairsInAnArray trivial

---
+++ Q2176-CountEqualAndDivisiblePairsInAnArray/src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+class Solution {
+    public int countPairs(int[] nums, int k) {
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j] && i * j % k == 0) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
