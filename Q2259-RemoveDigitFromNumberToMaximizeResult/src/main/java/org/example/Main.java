376f925e4c09296fde3c39ed9141ca7cb1627800
Minevitably <minevitably@gmail.com>
2026-04-11 23:40:18 +0800

    Q2259-RemoveDigitFromNumberToMaximizeResult easy

---
+++ Q2259-RemoveDigitFromNumberToMaximizeResult/src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+
+class Solution {
+    public String removeDigit(String number, char digit) {
+        int n = number.length();
+        int j = number.lastIndexOf(digit);
+
+        for (int i = 0; i < n; i++) {
+            if (number.charAt(i) == digit && (i + 1 == n || digit < number.charAt(i + 1))) {
+                j = i;
+                break;
+            }
+        }
+
+        return number.substring(0, j) + number.substring(j + 1);
+    }
+}
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
