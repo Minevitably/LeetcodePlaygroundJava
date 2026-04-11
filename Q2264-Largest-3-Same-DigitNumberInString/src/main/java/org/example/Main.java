3295652b34948d1cb0a9be62485f405c45a5d95a
Minevitably <minevitably@gmail.com>
2026-04-11 23:55:39 +0800

    Q2264-Largest-3-Same-DigitNumberInString easy

--- Q2264-Largest-3-Same-DigitNumberInString/src/main/java/org/example/Main.java
+++ Q2264-Largest-3-Same-DigitNumberInString/src/main/java/org/example/Main.java
@@ -2,16 +2,19 @@ package org.example;
 
 
 class Solution {
-    public int countPrefixes(String[] words, String s) {
-        int count = 0;
+    public String largestGoodInteger(String num) {
+        String ans = "";
+        int n = num.length();
 
-        for (String word: words) {
-            if (s.indexOf(word) == 0) {
-                count++;
+        for (int i = 0; i < n - 2; i++) {
+            if (num.substring(i, i + 3).equals(String.valueOf(num.charAt(i)).repeat(3))) {
+                if (ans.isEmpty() || ans.compareTo(num.substring(i, i + 3)) < 0) {
+                    ans = num.substring(i, i + 3);
+                }
             }
         }
 
-        return count;
+        return ans;
     }
 }
 
@@ -19,6 +22,7 @@ class Solution {
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
