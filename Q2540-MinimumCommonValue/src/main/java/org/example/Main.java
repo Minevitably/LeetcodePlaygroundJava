2a5101b5dae35bf7e12b4092f2ded92c0d73d8f9
Minevitably <minevitably@gmail.com>
2026-05-24 22:17:13 +0800

    Q2540-MinimumCommonValue trivial

---
+++ Q2540-MinimumCommonValue/src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        
+        for (int num: nums1) {
+            for (int v: nums2) {
+                if (num == v) {
+                    return num;
+                }
+                if (num < v) {
+                    break;
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
